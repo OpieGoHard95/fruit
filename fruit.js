63fd4621c8a7b91806478d02ad5f4b3876fcd0f6
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-16 12:14:52 -0500

    Attempt at the problem. I believe it's correct

---
+++ fruit.js
@@ -0,0 +1,13 @@
+// Coded by Jared Smith
+// Fruit JSON problem from Ryan's test
+// 2/16/15
+
+[
+  {"fruit": "apple", "color": "red"}
+  {"fruit": "orange", "color": "orange"}
+  {"fruit": "banana", "color": "yellow"}
+  {"fruit": "pear", "color": "green"}
+  {"fruit": "tomato", "color": "red"}
+]
+
+
